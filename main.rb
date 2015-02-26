5f51163e0e770f01663f43de8975257a02ae82ff
alter <root@localhost>
2015-02-26 22:30:08 +0300

    added new classes and new methods

--- main.rb
+++ main.rb
@@ -1,32 +1,45 @@
 #!/usr/bin/env ruby
 require_relative( 'mutant' )
 
-mob1 = Mutant.new('mob1',100,10)
-mob2 = Mutant.new('mob2',90,12)
+mob1 = Fighter.new('Red fighter', rand(90..100), rand(8..13))
+mob2 = Fighter.new('Green fighter', rand(90..100), rand(8..13))
+mob3 = Healer.new('Green healer', rand(20..30), rand(3..15))
+mob4 = Fighter.new()
+mob5 = Fighter.new()
 
-puts "#{mob1.name} hp: #{mob1.hp} damage: #{mob1.damage}"
-puts "#{mob2.name} hp: #{mob2.hp} damage: #{mob2.damage}"
+puts "#{mob4.name} hp: #{mob4.hp} damage: #{mob4.damage}"
+puts "#{mob5.name} hp: #{mob5.hp} damage: #{mob5.damage}"
 
-def fight(mob1, mob2)
-  while mob2.hp > 0 or mob1.hp > 0 do
-    mob1.stroke(mob2)
-    puts ">#{mob2.name} still has hp: #{mob2.hp}"
-    if mob2.hp <= 0
-      puts ">#{mob2.name} was killed"
-      mob2.status = "dead"
-      break
+def check_if_alive(mob)
+    puts ">#{mob.name} still has hp: #{mob.hp}"
+    if mob.hp <= 0
+      puts "  #{mob.name} was killed"
+      mob.status = Status::DEAD
     end
+end
+
+def check_status(mob)
+    mob.status = Status::WOUNDED if mob.hp < mob.max_hp and mob.status != Status::WOUNDED
+end
+
+def fight(mob1, mob2, mob3)
+  mob1_max_hp = mob1.hp
+  mob2_max_hp = mob2.hp
+  while mob2.status != Status::DEAD and mob1.status != Status::DEAD do
+    mob1.stroke(mob2)
+    check_status(mob2)
+    check_if_alive(mob2)
+    break if mob2.status == Status::DEAD
 
     mob2.stroke(mob1)
-    puts "<#{mob1.name} still has hp: #{mob1.hp}"
-    if mob1.hp <=0
-      puts "<#{mob1.name} was killed"
-      mob1.status = "dead"
-      break
-    end
+    check_status(mob1)
+    check_if_alive(mob1)
+    break if mob1.status == Status::DEAD
+
+    mob3.heal(mob2)
   end
 end
 
-fight(mob1, mob2)
-puts "#{mob1.name} status: #{mob1.status}"
-puts "#{mob2.name} status: #{mob2.status}"
+fight(mob4, mob5, mob3)
+puts "#{mob4.name} status: #{mob4.status_readable}"
+puts "#{mob5.name} status: #{mob5.status_readable}"
--- mutant.rb
+++ mutant.rb
@@ -1,17 +1,72 @@
-class Mutant
-  attr_accessor :name
-  attr_accessor :hp
-  attr_accessor :damage
-  attr_accessor :status
-  
-  def initialize(name, hp, damage) 
-    @name = name
-    @hp = hp
-    @damage = damage
-    @status = 'alive'
-  end 
+module Status
+  ALIVE   = 1
+  WOUNDED = 0
+  DEAD    = -1
+end
 
-  def stroke(enemy_obj)
-    enemy_obj.hp -= damage
-  end 
+class Mutant
+  include Status
+  attr_accessor :name
+  attr_accessor :max_hp
+  attr_accessor :hp
+  attr_accessor :status
+
+  def initialize(name="Mutant_id#{rand(0..99999999999)}", hp=95, damage=10)
+    @name = name
+    @max_hp = @hp = hp
+    @damage = damage
+    @status = Status::ALIVE
+    @heal = 3
+  end
+
+  def status_readable
+    case @status
+    when Status::ALIVE
+      'ALIVE'
+    when Status::WOUNDED
+      'WOUNDED'
+    when Status::DEAD
+      'DEAD'
+    end
+  end
+
+  def dead
+    @status = Status::DEAD
+  end
+
+  def is_dead?
+    if @status == Status::DEAD
+      true
+    else
+      false
+    end
+  end
+
+  def wounded
+    @status = Status::WOUNDED
+  end
+
+  def is_wounded?
+    if @status == Status::WOUNDED
+      true
+    else
+      false
+    end
+  end
+end
+
+class Fighter < Mutant
+  attr_accessor :damage
+
+  def stroke(object)
+    object.hp -= @damage
+  end
+end
+
+class Healer < Mutant
+  attr_accessor :heal
+
+  def heal(object)
+    object.hp += @heal# if object.is_wounded?
+  end
 end
